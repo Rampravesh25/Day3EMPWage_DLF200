00a66ca124a27175c32451dced8b7dbebbeadb9d
Rampravesh25 <rampraveshmallah@gmail.com>
2022-09-09 11:00:17 +0530

    Added:Daily Wages

--- src/com/bridgelabz/EmpWage.java
+++ src/com/bridgelabz/EmpWage.java
@@ -4,12 +4,21 @@ public class EmpWage {
     public static void main(String[] args) {
         System.out.println("Welcome Employees");
         int fullTime = 1;
+        int perHoursWage = 20;
+
         double check = Math.floor(Math.random() * 10) % 2;
         if (check == fullTime) {
             System.out.println("Emp is Present");
+            int Hrs = 8;
+            int salary = perHoursWage * Hrs;
+            System.out.println("Salary in Amount:  " + salary);
         } else {
             System.out.println("Emp is not Present");
+            int Hrs = 0;
+            int salary = perHoursWage * Hrs;
+            System.out.println("   " + salary);
         }
+
 
     }
 }
