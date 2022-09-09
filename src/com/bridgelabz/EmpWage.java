3640c5bb270df4b2e73b4127a2966dc11e5b12a6
Rampravesh25 <rampraveshmallah@gmail.com>
2022-09-09 10:53:36 +0530

    Added:Attendence sheet

--- src/com/bridgelabz/EmpWage.java
+++ src/com/bridgelabz/EmpWage.java
@@ -2,6 +2,14 @@ package com.bridgelabz;
 
 public class EmpWage {
     public static void main(String[] args) {
-        System.out.println("Enter");
+        System.out.println("Welcome Employees");
+        int fullTime = 1;
+        double check = Math.floor(Math.random() * 10) % 2;
+        if (check == fullTime) {
+            System.out.println("Emp is Present");
+        } else {
+            System.out.println("Emp is not Present");
+        }
+
     }
 }
